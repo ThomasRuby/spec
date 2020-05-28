83edba1fc610f56326de94ccb21cdd632b3433a8
trubiano <trubiano@totoro.irisa.fr>
2020-05-28 15:39:53 +0200

    importing more from valid.ml

--- interpreter/valid/valid.mli
+++ interpreter/valid/valid.mli
@@ -1,3 +1,16 @@
 exception Invalid of Source.region * string
 
 val check_module : Ast.module_ -> unit (* raises Invalid *)
+
+type ellipses = NoEllipses | Ellipses
+type infer_stack_type = ellipses * Types.value_type option list
+
+val peek : int -> 'a * 'b option list -> 'b option
+
+val check_unop : Ast.unop -> Source.region -> unit
+
+(* val check_memop : context -> 'a Ast.memop -> ('a option -> Types.pack_size option) -> Source.region -> unit *)
+
+val type_cvtop : Source.region -> Ast.cvtop -> Types.value_type * Types.value_type
+
+
