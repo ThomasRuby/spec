a896af980b611c3a4e6ae7e67742898562699e79
trubiano <trubiano@totoro.irisa.fr>
2020-06-01 03:52:54 +0200

    exporting more methodes from eval

--- interpreter/exec/eval.mli
+++ interpreter/exec/eval.mli
@@ -8,3 +8,14 @@ exception Exhaustion of Source.region * string
 
 val init : Ast.module_ -> extern list -> module_inst (* raises Link, Trap *)
 val invoke : func_inst -> value list -> value list (* raises Trap *)
+
+val create_table : module_inst -> Ast.table -> table_inst
+
+val create_memory : module_inst -> Ast.memory -> memory_inst
+
+val create_global : module_inst -> Ast.global -> global_inst
+
+val func_elem : module_inst -> int32 Source.phrase -> Table.index -> Source.region -> Instance.func_inst
+
+val type_ : module_inst -> int32 Source.phrase -> Types.func_type
+val block_type : module_inst -> Ast.block_type -> Types.func_type
